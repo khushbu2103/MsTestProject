7711796d00b8c5f55a71ca21212f9289e86a1c90
khushbu <khushbusoni475@gmail.com>
2023-09-11 20:45:40 +0530

    [khushbu] Adding test case program

---
+++ MsTestProject/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsTestProject
+{
+    public class Calculator
+    {
+        public int AddtwoNumbers(int first, int second)
+        {
+            return first + second;
+            Console.ReadLine();
+        }
+        public int DifferenceOftwoNumbers(int first, int second)
+        {
+            return Math.Abs(first - second);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ MsTestProject1/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsTestProject;
+using System;
+using System.Diagnostics;
+
+namespace MsTestProject1
+{
+    [TestClass]
+    public class CalculatorTestClass
+    {
+        [TestMethod]
+        [TestCategory("Addition")]
+        public void Given_TwoNumbers_should_Return_Sum()
+        {
+            ///AAA Methodology
+            ///Arrenge
+            int x =10;
+            int y = 20;
+            int expected = 30;
+            Calculator calculator = new Calculator();
+
+            //act
+            int actual = calculator.AddtwoNumbers(x, y);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //2nd type- we can write this program like this
+        //public class CalculatorTestClass
+        //{
+        //    [TestMethod]
+        //    [DataRow(10, 20, 30)]
+        //    [DataRow(30, 20, 50)]
+        //    [DataRow(30, 20, 30)]
+        //    public void Given_TwoNumbers_should_Return_Sum(int x, int y, int expected)
+        //    {
+        //        ///AAA Methodology
+        //        ///Arrenge
+
+        //        Calculator calculator = new Calculator();
+
+        //        //act
+        //        int actual = calculator.AddtwoNumbers(x, y);
+
+        //        //assert
+        //        Assert.AreEqual(expected, actual);
+        //    }
+        //}
+
+        //2nd test case (difference of two numbers)
+        [TestMethod]
+        [DataRow(10, 20, 30)]
+        [DataRow(30, 20, 50)]
+        [DataRow(10, 20, 30)]
+        [TestCategory("Substraction")]
+        public void Given_TwoNumbers_should_Return_Difference(int x, int y, int expected)
+        {
+            ///AAA Methodology
+            ///Arrenge
+
+            Calculator calculator = new Calculator();
+
+            //act
+            int actual = calculator.AddtwoNumbers(x, y);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+   
+
+}
